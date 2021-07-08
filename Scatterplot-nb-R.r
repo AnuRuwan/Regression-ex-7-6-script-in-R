69ef9a9696fe6f86ad113d49f75ac3b25aece00b
Anushka <anushka.srp@gmail.com>
2021-07-08 15:38:47 +0000

    next commits

--- Scatterplot-nb-R.r
+++ Scatterplot-nb-R.r
@@ -1,11 +1,18 @@
+# **Reading the dataframe**
+
 Regdata <- read.csv("regrex1.csv")
 x <- Regdata$x
 y <- Regdata$y
 
 print(Regdata)
 
+# **Creating a scatterplot**
+
 png('scatterplot-Script-from-R.png')
 plot(x, y, main = "Regex1_scatterplot_R", col = "blue")
+
+
+# ** creating a linearmodel**
 
 png('linearmodeling-Script-from-R.png')
 plot(x, y, main = "regrex1_scatterplot_R_linermodeling", col = "blue")
