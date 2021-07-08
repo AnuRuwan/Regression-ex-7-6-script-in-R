d6809b9d9c7b75ce0d5e2a0530e21758f3cacb5e
Anushka <anushka.srp@gmail.com>
2021-07-08 15:57:28 +0000

    scatterplot

--- Scatterplot-nb-R.r
+++ Scatterplot-nb-R.r
@@ -1,22 +1,22 @@
-# **R-defining the argument**
+# R-defining the argument
 
 args = commandArgs(trailingOnly=TRUE)
 
 
-# **Reading the dataframe using argument**
+# Reading the dataframe using argument
 Regdata <- read.csv(args[1])
 x <- Regdata$x
 y <- Regdata$y
 
 print(Regdata)
 
-# **Creating a scatterplot**
+# Creating a scatterplot
 
 png('scatterplot-Script-from-R.png')
 plot(x, y, main = "Regex1_scatterplot_R", col = "blue")
 
 
-# ** creating a linearmodel**
+# creating a linearmodel
 
 png('linearmodeling-Script-from-R.png')
 plot(x, y, main = "regrex1_scatterplot_R_linermodeling", col = "blue")
